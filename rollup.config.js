33609c0b091dd2a3736bfd225eab2d3662a20643
Robert Ing <robertming@gmail.com>
2021-01-12 10:23:52 -0500

    Update integration wrapper dependency, update rollup configuration (#16)
    - Updating the dependency fixes a bug for building server side rendering in a self hosted app where `window` is unavailable

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,50 @@
-import base from './node_modules/@mparticle/web-kit-wrapper/rollup.base';
+import {
+    production,
+    testEndToEnd,
+} from './node_modules/@mparticle/web-kit-wrapper/rollup.base';
 
-export default [
-    {
-        input: base.input,
+const { ENVIRONMENT } = process.env;
+const initialization = require('./src/initialization');
+
+const productionBuilds = {
+    iife: {
+        input: production.input,
         output: {
-            ...base.output,
+            ...production.output,
             format: 'iife',
-            file: 'build/GoogleTagManager-Kit.js',
-            name: 'mpGoogleTagManagerKit'
+            file: `dist/${initialization.name}-Kit.iife.js`,
+            name: `${initialization.name}Kit`,
         },
-        plugins: [...base.plugins]
+        plugins: [...production.plugins],
     },
-    {
-        input: base.input,
+    cjs: {
+        input: production.input,
         output: {
-            ...base.output,
-            format: 'iife',
-            file: 'dist/GoogleTagManager-Kit.iife.js',
-            name: 'mpGoogleTagManagerKit'
-        },
-        plugins: [...base.plugins]
-    },
-    {
-        input: base.input,
-        output: {
-            ...base.output,
+            ...production.output,
             format: 'cjs',
-            file: 'dist/GoogleTagManager-Kit.common.js',
-            name: 'mpGoogleTagManagerKit'
+            file: `dist/${initialization.name}-Kit.common.js`,
+            name: `${initialization.name}Kit`,
         },
-        plugins: [...base.plugins]
-    }
-];
+        plugins: [...production.plugins],
+    },
+};
+
+const testEndToEndBuild = {
+    input: testEndToEnd.input,
+    output: {
+        ...testEndToEnd.output,
+        format: 'iife',
+        file: 'test/end-to-end-testapp/build/compilation.js',
+        name: `${initialization.name}Kit`,
+    },
+    plugins: [...testEndToEnd.plugins],
+};
+
+let selectedBuilds = [];
+if (ENVIRONMENT === 'production') {
+    selectedBuilds.push(productionBuilds.iife, productionBuilds.cjs);
+} else if (ENVIRONMENT === 'testEndToEnd') {
+    selectedBuilds.push(testEndToEndBuild);
+}
+
+export default selectedBuilds;
